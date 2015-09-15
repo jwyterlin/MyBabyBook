7a2c53a06e4e4ddba6f295c857dade3f00549602
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-09-14 22:13:07 -0300

    Initial project

---
+++ MyBabyBook/MyBabyBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBabyBook
+//
+//  Created by Jhonathan Wyterlin on 14/09/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyBabyBook/MyBabyBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyBabyBook
+//
+//  Created by Jhonathan Wyterlin on 14/09/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
