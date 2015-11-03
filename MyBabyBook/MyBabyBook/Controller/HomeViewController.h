38ec5f130fdd79f405757e6548f47a90362d081a
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-11-02 23:42:54 -0200

    Refactoring ViewController to HomeViewController

--- MyBabyBook/MyBabyBook/Controller/HomeViewController.h
+++ MyBabyBook/MyBabyBook/Controller/HomeViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HomeViewController.h
 //  MyBabyBook
 //
 //  Created by Jhonathan Wyterlin on 14/09/15.
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface HomeViewController : UIViewController
 
 
 @end
---
+++ MyBabyBook/MyBabyBook/Controller/HomeViewController.m
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.m
+//  MyBabyBook
+//
+//  Created by Jhonathan Wyterlin on 14/09/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
